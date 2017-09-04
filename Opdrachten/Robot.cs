5052966f53f696f57c2a0a727ce012a709ee0fe7
Krotau <tla.smeets@outlook.com>
2017-09-04 19:40:57 +0200

    Begonnen aan Robot opdracht week 1

---
+++ Opdrachten/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Opdrachten
+{
+    public class Robot
+    {
+        private int plaats;
+        private int richting;
+        private int links = -1;
+        private int rechts = 1;
+
+        public Robot()
+        {
+            Console.WriteLine("Thank you for creating me! I will be a good robot. :D");
+        }
+
+
+    }
+}
