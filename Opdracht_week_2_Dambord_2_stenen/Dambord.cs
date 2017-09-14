913466239e7b6a860ec9a068492852190697bdab
Krotau <tla.smeets@outlook.com>
2017-09-14 13:43:41 +0200

    Started Excercise

---
+++ Opdracht_week_2_Dambord_2_stenen/Dambord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Opdracht_week_2_Dambord_2_stenen
+{
+    public class Dambord
+    {
+        public Dambord()
+        {
+        }
+    }
+}
---
+++ Opdracht_week_2_Dambord_2_stenen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opdracht_week_2_Dambord_2_stenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sup ma boi!");
+        }
+    }
+}
